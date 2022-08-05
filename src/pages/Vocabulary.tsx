282aa62204d88985afe759812f499ba4d73aa871
galiya <gkey-587@mail.ru>
2022-08-05 20:23:48 +0500

    почти доделанный вокабуляри...

--- src/pages/Vocabulary.tsx
+++ src/pages/Vocabulary.tsx
@@ -7,8 +7,10 @@ function Vocabulary() {
 
   const [data, setData] = useState<any[]>([]);
   const [activeWord, setActiveWord] = useState(0);
+  const [previousWord, setPreviousWord] = useState(-1);
 
   const [points, setPoints] = useState(0);
+  const [dataLeft, setDataLeft] = useState(true);
   const [feedbackVisible, setFeedbackvisible] = useState(false);
   const [isCorrect, setCorrect] = useState(false);
 
@@ -23,14 +25,26 @@ function Vocabulary() {
   }, [])
 
   function checkAnswer(answer: string) {
+    setPreviousWord(previousWord+1);
     setFeedbackvisible(true);
     if(answer === data[activeWord].answer) {
       setCorrect(true);
+      setPoints(points+1)
     }
     else {
       setCorrect(false);
     }
-    setActiveWord(activeWord+1)
+    checkDataLeft()
+  }
+
+  function checkDataLeft(){
+    if(data.length > activeWord + 1) {
+      setActiveWord(activeWord+1)
+    }
+    else {
+      setDataLeft(false)
+      setActiveWord(activeWord+1)
+    }
   }
 
   return (
@@ -49,55 +63,94 @@ function Vocabulary() {
                 </button>
             </div>
             <div className="vocabulary__panel panel">
-                <div>
-                {feedbackVisible && <div className="vocabulary__feedback feedback">
-                  <span className={isCorrect ? "feedback__info feedback__info_correct" : "feedback__info feedback__info_wrong"}>{isCorrect ? "correct" : "wrong"}!</span>
+                <div className="panel__main">
+                  {feedbackVisible && <div className="vocabulary__feedback feedback">
+                    <span className={isCorrect ? "feedback__info feedback__info_correct" : "feedback__info feedback__info_wrong"}>{isCorrect ? "correct" : "wrong"}!</span>
 
-                  (<span className="feedback__word">{data[activeWord].word}</span><span className="feedback__answer">{data[activeWord].answer}</span>)
-                </div>}
-                <div className="vocabulary__task task">
-                    <h3 className="task__title">
-                        <span className="task__word">
-                          {data[activeWord].word}
-                        </span>
-                        <span className="task__word-info">
-                            ({data[activeWord].info})
-                        </span>
-                    </h3>
-                    <p className="task__explanation">
-                        <span className="task__flag"><img src={englishFlag} alt="English flag" /></span>
-                        <span className="task__flag"><img src={englishFlag} alt="English flag" /></span>
-                        <span className="task__explanation-text">is translated to English:</span>
-                    </p>
-                    <div className="task__wrapper">
-                      {data[activeWord].answers.map((answer:string, index:number) => {
-                        return(
-                        <div className="task__container">
-                            <div className="task__text" onClick={() => checkAnswer(answer)}>
-                              <span className="task__number">{index+1}</span>
-                              <div className="task__answer">{answer}</div>
-                            </div>
+                    (<span className="feedback__word">{data[previousWord].word}</span><span className="feedback__answer">{data[previousWord].answer}</span>)
+                  </div>}
+                  {dataLeft ?
+                  <div className="vocabulary__task task">
+                      <h3 className="task__title">
+                          <span className="task__word">
+                            {data[activeWord].word}
+                          </span>
+                          <span className="task__word-info">
+                              ({data[activeWord].info})
+                          </span>
+                      </h3>
+                      <p className="task__explanation">
+                          <span className="task__flag"><img src={englishFlag} alt="English flag" /></span>
+                          <span className="task__flag"><img src={englishFlag} alt="English flag" /></span>
+                          <span className="task__explanation-text">is translated to English:</span>
+                      </p>
+                      {vocabularyType == 0
+                        ? <div className="task__wrapper">
+                        {data[activeWord].answers.map((answer:string, index:number) => {
+                          return(
+                          <div className="task__container">
+                              <div className="task__text" onClick={() => checkAnswer(answer)}>
+                                <span className="task__number">{index+1}</span>
+                                <div className="task__answer">{answer}</div>
+                              </div>
+                          </div>
+                          )
+                          })
+                        }
+                      </div>
+                      : <div className='vocabulary__active active'>
+                          <div className="active__wrapper">
+                            {/* {data[activeWord].word.length} */}
+                            {`${data[activeWord].word.length  * 25 + 16}px`}
+                            <input style={{width: `${data[activeWord].word.length  * 25 + 41}px`}} type="text" className="active__insert"  />
+                            {/* <p className="active__letters">
+                              {data[activeWord].word.split("").map((letter: string, index: number) => {
+                                return letter == " " ? <span className="active__letter active__letter_gap" key={index}>{0} </span>: <input className="active__letter" key={index} />
+                              })}
+                            </p> */}
+                          </div>
                         </div>
-                        )
-                    })
-                  }
-                    </div>
-                    <div className="task__prompts">
-                        <div className="task__propmpts-keybord">
-                            <span>Without a mouse: Select with keys 1 to 6!</span>
-                        </div>
-                        <div className="task__propmpts-send">
-                            <span>Something is wrong? <a href="#">Report error</a></span>
-                        </div>
-                    </div>
-                </div>
+                      }
+                  </div>
+                  : <div className="vocabulary__task task">
+                      <h3 className="task__title">
+                        There're no more words...
+                      </h3>
+                    </div>}
+                  <div className="task__prompts">
+                      {vocabularyType == 0 && <div className="task__propmpts-keybord">
+                          <span>Without a mouse: Select with keys 1 to 6!</span>
+                      </div>}
+                      <div className="task__propmpts-send">
+                          <span>Something is wrong? <a href="#">Report error</a></span>
+                      </div>
+                  </div>
                 </div>
                 <div className="vocabulary__statistics statistics">
-                    <h5 className="statistics__title">Statistics</h5>
-                    <p className="statistics__attempts"><span>3</span> attempts</p>
-                    <div className="statistics__display">
-                        <div className="statistics__bar"></div>
-                        <span className="statistics__percent">20</span>
+                    <div className="statistics__block">
+                      <h5 className="statistics__title">Statistics</h5>
+                      <p className="statistics__attempts"><span>{activeWord}</span> attempts</p>
+                      {activeWord ? <div className="statistics__display">
+                          <div className="statistics__bar">
+                            <div style={{width: `${Math.floor(points/activeWord*100)}%`}} className={points == activeWord ? "statistics__bar-content statistics__bar-content_full" : "statistics__bar-content"}></div>
+                          </div>
+                          <span className="statistics__percent">{Math.floor(points/activeWord*100)}</span>
+                      </div>
+                      : ""}
+                    </div>
+                    <div className="statistics__block">
+                      <h5 className="statistics__title">Words</h5>
+                      <p className="statistics__words">Words in total: <span>{activeWord}</span>
+                        <ul className="statistics__review">
+                          <li>last right: <span>{points}</span></li>
+                          <li>last wrong: <span>{activeWord - points}</span></li>
+                          <li>newly learned words: <span>0</span></li>
+                        </ul>
+                      </p>
+                    </div>
+                    <div className="statistics__block">
+                      <h5 className="statistics__title">change direction</h5>
+                      <button className="statistics__direction task__text" onClick={() => vocabularyType == 0 ? setVocabularyType(1) : setVocabularyType(0)}><span>{vocabularyType == 0 ? 'active' : "passive"} vocabulary</span></button>
                     </div>
                 </div>
             </div>
